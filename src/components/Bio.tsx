451314f6876e68f258ab9aef0038f9e869785dfe
Martighervas <ghervasmartin@gmail.com>
2024-01-03 15:19:06 +0100

    margin for responsive

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -14,7 +14,7 @@ const Bio = () => {
     return (
         <div className="flex flex-col w-full justify-start lg:justify-center">
             <SocialMedia infoSelect={infoHandler}/>
-            <div className="flex flex-col lg:flex-row justify-between h-full lg:h-3/4">
+            <div className="flex flex-col ml-2 lg:m-0 lg:flex-row justify-between h-full lg:h-3/4">
                 <div className="lg:w-1/2">
                     <h1 className="text-5xl lg:text-8xl mb-10">{info === 'Bio' ? db.Bio.title.en : info}</h1>
                     <div className="border-l-4 border-gray-700 hover:border-bordergreen transition-all duration-300 rounded-lg p-3">
