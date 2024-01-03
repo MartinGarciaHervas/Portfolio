eb863ee3b1b35cd741d2188279686d3104765c08
Martighervas <ghervasmartin@gmail.com>
2024-01-03 13:52:14 +0100

    projects show OK

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,17 @@
 import db from '../utils/db.json'
 
-const Projects = () => {
+interface Props {
+    infoSelect: (proyecto:string)=>void
+}
+
+const Projects: React.FC<Props> = ({infoSelect}) => {
     return (
         <div className="flex flex-col lg:items-center mt-20 lg:mt-0 lg:w-1/2">
             <div>
                 <h1 className="text-4xl mb-10">My Projects</h1>
                 <div className="flex flex-col border-l-4 border-t-4 border-gray-700 hover:border-bordergreen transition-all duration-300 rounded-lg p-3">
                     {db.Projects.map((project, index) => (
-                        <p key={index}>{project.title}</p>
+                        <button onClick={()=>{infoSelect(project.title)}} key={index}>{project.title}</button>
                     ))}
                 </div>
             </div>
