e15412066d59c0ab953ae8f1ca8bde18ad3785e8
Martighervas <ghervasmartin@gmail.com>
2024-01-03 10:09:43 +0100

    making it responsive

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -4,9 +4,9 @@ import SocialMedia from "../components/SocialMedia"
 
 const Home = () => {
     return (
-        <div className="flex flex-col justify-center p-7 w-3/4 h-3/4 border-r-4 border-bordergreen hover:border-gray-700 transition-all duration-300 rounded-xl">
+        <div className="flex flex-col justify-center lg:p-7 lg:w-3/4 lg:h-3/4 border-r-4 border-bordergreen hover:border-gray-700 transition-all duration-300 rounded-xl">
             <SocialMedia />
-            <div className="flex">
+            <div className="flex wrap">
                 <Bio />
                 <Projects />
             </div>
