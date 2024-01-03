27b24ecd2c5aa35e439726d31f2eaca36259219a
Martighervas <ghervasmartin@gmail.com>
2024-01-03 13:21:45 +0100

    changing structure so i can apply logic

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,15 +1,11 @@
 import Bio from "../components/Bio"
-import Projects from "../components/Projects"
 import SocialMedia from "../components/SocialMedia"
 
 const Home = () => {
     return (
         <div className="flex flex-col justify-center lg:p-7 lg:w-3/4 lg:h-3/4 pt-4 border-t-4 border-r-4 border-bordergreen hover:border-gray-700 transition-all duration-300 rounded-xl">
             <SocialMedia />
-            <div className="flex flex-col lg:flex-row items-start ml-5">
-                <Bio />
-                <Projects />
-            </div>
+            <Bio />
         </div>
     )
 }
--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,16 +1,21 @@
+import Projects from "./Projects"
+
 const Bio = () => {
 
     
 
     return (
-        <div className="flex justify-start lg:w-1/2">
-            <div>
+        <div className="flex flex-col items-start lg:flex-row">
+            <div className="lg:w-1/2">
                 <h1 className="text-5xl lg:text-8xl mb-10">Welcome!</h1>
                 <div className="border-l-4 border-gray-700 hover:border-bordergreen transition-all duration-300 rounded-lg p-3">
                     <p className="mb-5">My name is Martín García Hervás, I'm a Full Stack Developer, with background experience of over 3 years in logistics and construction. I have worked on projects involving design and
                         development in both Front End and Back End.</p>
                     <p>My Soft-Skills include strong organizational ability, teamwork, and leadership.</p>
                 </div>
+            </div>
+            <div className="flex lg:w-1/2 lg:h-full lg:justify-center lg:items-center">
+                <Projects/>
             </div>
         </div>
     )
