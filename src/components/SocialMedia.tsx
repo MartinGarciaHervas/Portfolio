4a5c5a11ca064c8ab98fc5c7bc2b3870b7a067b8
Martighervas <ghervasmartin@gmail.com>
2024-01-05 13:37:13 +0100

    language icons OK

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -2,6 +2,8 @@ import github from '../assets/MdiGithub.svg'
 import linkedin from '../assets/MdiLinkedin.svg'
 import gmail from '../assets/CibGmail (1).svg'
 import logo from '../../public/logo white.svg'
+import spain from '../assets/TwemojiFlagSpain.svg'
+import UK from '../assets/TwemojiFlagUnitedKingdom.svg'
 
 interface Props {
     infoSelect: (proyecto:string)=>void
@@ -11,7 +13,7 @@ interface Props {
 const SocialMedia:React.FC<Props> = ({infoSelect, languageInfo}) => {
     return (
         <div className='flex justify-between mt-2 mb-6 w-full'>
-            <div className='rounded-full pt-1 pr-1 hover:bg-bordergreen transition-all duration-500'>
+            <div className='rounded-full pt-1 pr-1 hover:bg-bordergreen transition-all duration-300'>
                 <button onClick={()=>{infoSelect('Bio')}} className='pl-2 pr-2'><img className='w-16' src={logo}/></button>
             </div>
             <div className='flex'>
@@ -20,7 +22,7 @@ const SocialMedia:React.FC<Props> = ({infoSelect, languageInfo}) => {
                         <img
                             src={gmail}
                             alt="gmail"
-                            className="w-8 hover:w-9 transition-all duration-500"
+                            className="w-8 hover:w-9 transition-all duration-300"
                         />
                     </a>
                 </div>
@@ -29,7 +31,7 @@ const SocialMedia:React.FC<Props> = ({infoSelect, languageInfo}) => {
                         <img
                             src={github}
                             alt="github"
-                            className="w-8 hover:w-9 transition-all duration-500"
+                            className="w-8 hover:w-9 transition-all duration-300"
                         />
                     </a>
                 </div>
@@ -38,12 +40,12 @@ const SocialMedia:React.FC<Props> = ({infoSelect, languageInfo}) => {
                         <img
                             src={linkedin}
                             alt="linkedin"
-                            className="w-8 hover:w-9 transition-all duration-500"
+                            className="w-8 hover:w-9 transition-all duration-300"
                         />
                     </a>
                 </div>
                 <div>
-                    <button onClick={()=>{languageInfo()}}>Esp-Eng</button>
+                    <button onClick={()=>{languageInfo()}}><div className='flex justify-between items-center h-7 w-20 m-0.5'><img className='w-7 hover:w-9 transition-all duration-300' src={spain}/><img className='w-7 hover:w-9 transition-all duration-300' src={UK}/></div></button>
                 </div>
             </div>
         </div>
